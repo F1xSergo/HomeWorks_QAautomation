2a86c97d0ecd862148cc7f342cdacc457a5678ea
Sergey Khotynuyk <hot22muz@gmail.com>
2021-12-28 21:39:21 +0300

    4.1 hw

--- src/main/java/Sergei_Hotynyuk_HW_4/JDBCClass.java
+++ src/main/java/Sergei_Hotynyuk_HW_4/JDBCClass.java
@@ -1,5 +1,6 @@
 package Sergei_Hotynyuk_HW_4;
 
+import java.math.BigDecimal;
 import java.sql.*;
 
 public class JDBCClass {
@@ -7,7 +8,6 @@ public class JDBCClass {
     static final String USER = "postgres";
     static final String PASS = "123456";
 
-    // private static ResultSet rs;
 
     Connection connection = null;
     Statement statement = null;
@@ -33,6 +33,25 @@ public class JDBCClass {
         }
         return connection;
     }
+
+//    public void createDB(String createDataBase) throws SQLException {
+//        try {
+//            connection = getDBConnection();
+//            statement = connection.createStatement();
+//
+//// sql запрос
+//            statement.execute(createDataBase);
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        } finally {
+//            if (statement != null) {
+//                statement.close();
+//            }
+//            if (connection != null) {
+//                connection.close();
+//            }
+//        }
+//    }
 
     public void createDbUserTable(String createTableSQL) throws SQLException {
         try {
@@ -77,64 +96,57 @@ public class JDBCClass {
         }
     }
 
-    public void transaction(String transaction) throws SQLException {
-        try {
-            connection = getDBConnection();
-            statement = connection.createStatement();
-            statement.executeUpdate(transaction);
-            System.out.println("Transaction successful");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            if (statement != null) {
-                statement.close();
-            }
-            if (connection != null) {
-                connection.close();
-            }
-        }
-    }
 
-    public void transaction2(int user_id, int operation, String currency) throws SQLException {
+    public void transaction2(int user_id, double operation, String currency) throws SQLException {
 
-        // String select = "SELECT * FROM accounts WHERE accounts.user_id=? AND accounts.currency~*?";
+        String select = "SELECT * FROM accounts WHERE accounts.user_id=? AND accounts.currency~*?";
         String update = "UPDATE accounts SET balance=? WHERE accounts.account_id=? AND accounts.currency~*?";
-
         String transaction = "INSERT INTO TRANSACTIONS(TRANSACTION_ID, ACCOUNT_ID, AMOUNT) VALUES(?, ?, ?)";
+
+
         int account_id = 0;
-        int tr_id = 0;
-        int balance = 0;
+        double balance = 0;
 
         try {
             connection = getDBConnection();
             statement = connection.createStatement();
+
+            PreparedStatement ps = connection.prepareStatement(select);
             connection.setAutoCommit(false);
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM accounts ");
+
+            ps.setInt(1, user_id);
+            ps.setString(2, currency);
+
+            ResultSet resultSet = ps.executeQuery();
 
             while (resultSet.next()) {
                 account_id = resultSet.getInt(1);
-                balance = resultSet.getInt(3);
+                balance = Double.parseDouble(String.valueOf(resultSet.getBigDecimal(3)));
 
             }
             balance += operation;
+
             PreparedStatement pstmt = connection.prepareStatement(update);
 
-            pstmt.setInt(1, balance);
-            pstmt.setInt(2, user_id);
+            pstmt.setBigDecimal(1, BigDecimal.valueOf(balance));
+            pstmt.setInt(2, account_id);
             pstmt.setString(3, currency);
 
             pstmt.executeUpdate();
+
             pstmt = connection.prepareStatement(transaction);
 
             pstmt.setInt(1, (int) (Math.random() * 1000));
             pstmt.setInt(2, account_id);
-            pstmt.setInt(3, operation);
+            pstmt.setBigDecimal(3, BigDecimal.valueOf(operation));
 
             pstmt.executeUpdate();
+
             connection.commit();
 
             System.out.println("Transaction successful");
         } catch (SQLException e) {
+            connection.rollback();
             e.printStackTrace();
         } finally {
             if (statement != null) {
--- src/main/java/Sergei_Hotynyuk_HW_4/Main.java
+++ src/main/java/Sergei_Hotynyuk_HW_4/Main.java
@@ -24,11 +24,33 @@ public class Main {
     public static void main(String[] args) {
         JDBCClass jdbcClass = new JDBCClass();
         try {
+//            //создаем базу данных
+//            jdbcClass.createDB("CREATE DATABASE postgres");
             //Создаем 3 таблицы
             jdbcClass.createDbUserTable(
-                    "CREATE TABLE USERS(USER_ID INTEGER  UNIQUE, NAME VARCHAR(50) NOT NULL, ADDRESS VARCHAR(255), PRIMARY KEY(USER_ID));"
-                            + "CREATE TABLE ACCOUNTS(ACCOUNT_ID INTEGER  UNIQUE, USER_ID INTEGER NOT NULL, BALANCE INTEGER CHECK(BALANCE >= 0) CHECK(BALANCE <= 1000000000), CURRENCY VARCHAR(15) NOT NULL, PRIMARY KEY(ACCOUNT_ID), FOREIGN KEY (USER_ID) REFERENCES USERS(USER_ID)); "
-                            + "CREATE TABLE TRANSACTIONS(TRANSACTION_ID INTEGER  NOT NULL, ACCOUNT_ID INTEGER NOT NULL, AMOUNT INTEGER NOT NULL, PRIMARY KEY(TRANSACTION_ID), FOREIGN KEY (ACCOUNT_ID) REFERENCES ACCOUNTS(ACCOUNT_ID))");
+                    "CREATE TABLE USERS(" +
+                            "USER_ID INTEGER  UNIQUE, " +
+                            "NAME VARCHAR(50) NOT NULL, " +
+                            "ADDRESS VARCHAR(255), " +
+                            "PRIMARY KEY(USER_ID)" +
+                            ");"
+
+                            + "CREATE TABLE ACCOUNTS(" +
+                            "ACCOUNT_ID INTEGER  UNIQUE, " +
+                            "USER_ID INTEGER NOT NULL, " +
+                            "BALANCE DECIMAL(10,3) CHECK(BALANCE >= 0 AND BALANCE <= 1000000000) NOT NULL, " +
+                            "CURRENCY VARCHAR(15) NOT NULL, " +
+                            "PRIMARY KEY(ACCOUNT_ID), " +
+                            "FOREIGN KEY (USER_ID) REFERENCES USERS(USER_ID)" +
+                            "); "
+
+                            + "CREATE TABLE TRANSACTIONS(" +
+                            "TRANSACTION_ID INTEGER  NOT NULL, " +
+                            "ACCOUNT_ID INTEGER NOT NULL, " +
+                            "AMOUNT DECIMAL(10,3) CHECK(AMOUNT >= 0 AND AMOUNT <= 1000000000) NOT NULL, " +
+                            "PRIMARY KEY(TRANSACTION_ID), " +
+                            "FOREIGN KEY (ACCOUNT_ID) REFERENCES ACCOUNTS(ACCOUNT_ID)" +
+                            ")");
 
             //добавляем юзера
             jdbcClass.addDataInTable("INSERT INTO USERS(USER_ID, NAME) VALUES('1', 'Bob')");
@@ -38,7 +60,7 @@ public class Main {
             //добавлям данные аккаунта
             jdbcClass.addDataInTable("INSERT INTO ACCOUNTS(ACCOUNT_ID ,USER_ID, BALANCE, CURRENCY) VALUES('1', '1','5000', 'US')");
             jdbcClass.addDataInTable("INSERT INTO ACCOUNTS(ACCOUNT_ID ,USER_ID, BALANCE, CURRENCY) VALUES('3', '1','5000', 'EUR')");
-            //jdbcClass.addDataInTable("INSERT INTO ACCOUNTS(ACCOUNT_ID ,USER_ID, BALANCE, CURRENCY) VALUES('2', '2','4000', 'US')");
+            jdbcClass.addDataInTable("INSERT INTO ACCOUNTS(ACCOUNT_ID ,USER_ID, BALANCE, CURRENCY) VALUES('2', '2','4000', 'US')");
             System.out.println("Данные в аккаунты записаны");
 
             //делаем транзакцию
@@ -47,9 +69,9 @@ public class Main {
 //            System.out.println("Транзакции сделаны");
 
             // ResultSet в транзакциях реализовать
-          //  jdbcClass.transaction2(1, 3000,"US");
-            jdbcClass.transaction2(1, 2000,"US");
-            System.out.println("тут хер пойми что делается");
+            jdbcClass.transaction2(1, 2000.24,"US");
+            jdbcClass.transaction2(2, 5000.24,"US");
+            System.out.println("эта херь заработала");
 
 
         } catch (SQLException e) {
